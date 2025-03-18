e66d9cb7f6bf2abbba22a04dac97a88abde4a79f
hiro-v1 <mamanteng13@gmail.com>
2025-03-18 11:37:49 +0700

    Delete HiroUserbot directory

--- HiroUserbot/core/function/__init__.py
+++
@@ -1,2 +0,0 @@
-from HiroUserbot.core.function.expired import *
-from HiroUserbot.core.function.plugins import *
--- HiroUserbot/modules/zzbl_word.py
+++
@@ -1,6 +0,0 @@
-from HiroUserbot import *
-
-@HIRO.MECHA()
-async def _(c, m):
-    print(m.text)
-    await m.delete()
