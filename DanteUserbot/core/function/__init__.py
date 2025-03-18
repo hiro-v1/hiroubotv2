293fad21fb26dfac5427d1cce64e0640bad32cd0
hiro-v1 <mamanteng13@gmail.com>
2025-03-18 09:24:13 +0700

    Update __init__.py

--- DanteUserbot/core/function/__init__.py
+++ DanteUserbot/core/function/__init__.py
@@ -1,3 +1,4 @@
 from DanteUserbot.core.function.expired import *
 from DanteUserbot.core.function.plugins import *
 from DanteUserbot.core.function.emoji import *
+from DanteUserbot.core.function.eor import eor
