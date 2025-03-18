86cfe6b8eebf086e310cfedcf9b79bd7bacda0d8
hiro-v1 <mamanteng13@gmail.com>
2025-03-18 09:00:45 +0700

    Update emoji.py

--- DanteUserbot/core/function/emoji.py
+++ DanteUserbot/core/function/emoji.py
@@ -12,3 +12,16 @@ def emoji(alias):
         "ubot": "<emoji id=5911100572508885928>♻️</emoji>",     # Example emoji for UBOT
     }
     return emojis.get(alias, "Emoji tidak ditemukan.")
+
+EMO = {
+    "bintang": "<emoji id=5911461474315802019>⭐</emoji>",
+    "loading": "<emoji id=5801044672658805468>✨</emoji>",
+    "proses": "<emoji id=6276248783525251352>🔄</emoji>",
+    "gagal": "<emoji id=6278161560095426411>❌</emoji>",
+    "done": "<emoji id=6278555627639801385>✅</emoji>",
+    "upload": "<emoji id=5911100572508885928>♻️</emoji>",
+    "berhasil": "<emoji id=6278555627639801385>✅</emoji>",
+    "ping": "<emoji id=5801044672658805468>✨</emoji>",
+    "mention": "<emoji id=5911461474315802019>⭐</emoji>",
+    "ubot": "<emoji id=5911100572508885928>♻️</emoji>",
+}
