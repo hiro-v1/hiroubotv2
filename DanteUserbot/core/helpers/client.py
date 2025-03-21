29b79efa8dcb7e16c1b009b623ac89bbf9c52fbd
hiro-v1 <mamanteng13@gmail.com>
2025-03-21 16:23:55 +0700

    Update client.py

--- DanteUserbot/core/helpers/client.py
+++ DanteUserbot/core/helpers/client.py
@@ -74,13 +74,18 @@ class DANTE:
         
     def BOT(command, filter=FILTERS.PRIVATE):
         def wrapper(func):
-            @bot.on_message(filters.command(command) & filter)
-            async def wrapped_func(client, message):
-                await func(client, message)
+             message_filters = filters.command(command, prefixes=["/"])
+             if filter:
+                message_filters &= filter  # Gunakan & untuk menggabungkan command dengan filter lainnya
 
-            return wrapped_func
+        @bot.on_message(message_filters)
+        async def wrapped_func(client, message):
+            await func(client, message)
+        
+        return wrapped_func
 
-        return wrapper
+    return wrapper
+
         
     def OWNER(func):
         async def function(client, message):
